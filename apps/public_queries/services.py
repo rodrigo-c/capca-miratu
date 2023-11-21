da14902d13e9121fcf829198d84025890cd34e18
Sergio Acuña <sergioacunao@protonmail.com>
2023-11-21 13:08:33 -0300

    chore(public_queries.services): add get_active_public_query_by_uuid

--- apps/public_queries/services.py
+++ apps/public_queries/services.py
@@ -1,6 +1,9 @@
 from uuid import UUID
 
+from django.utils import timezone
+
 from apps.public_queries.lib.dataclasses import PublicQueryData
+from apps.public_queries.lib.exceptions import PublicQueryDoesNotExist
 from apps.public_queries.providers import public_query as public_query_providers
 from apps.utils.dataclasses import build_dataclass_from_model_instance
 
@@ -8,5 +11,25 @@ from apps.utils.dataclasses import build_dataclass_from_model_instance
 def get_public_query_by_uuid(uuid: UUID) -> PublicQueryData:
     public_query = public_query_providers.get_public_query_by_uuid(uuid=uuid)
     return build_dataclass_from_model_instance(
-        klass=PublicQueryData, instance=public_query, uuid=public_query.id
+        klass=PublicQueryData,
+        instance=public_query,
+        uuid=public_query.id,
+        image=public_query.image.url if public_query.image else None,
     )
+
+
+def get_active_public_query_by_uuid(uuid: UUID) -> PublicQueryData:
+    now = timezone.now()
+    public_query = public_query_providers.get_public_query_by_uuid(
+        uuid=uuid, active=True
+    )
+    is_after_start = public_query.start_at is None or public_query.start_at < now
+    is_before_end = public_query.end_at is None or public_query.end_at > now
+    if is_after_start and is_before_end:
+        return build_dataclass_from_model_instance(
+            klass=PublicQueryData,
+            instance=public_query,
+            uuid=public_query.id,
+            image=public_query.image.url if public_query.image else None,
+        )
+    raise PublicQueryDoesNotExist
--- apps/public_queries/tests/test_services.py
+++ apps/public_queries/tests/test_services.py
@@ -1,7 +1,14 @@
+from datetime import datetime
+
 import pytest
+from django.utils.timezone import make_aware
+
+from freezegun import freeze_time
 
 from apps.public_queries import services
 from apps.public_queries.lib.dataclasses import PublicQueryData
+from apps.public_queries.lib.exceptions import PublicQueryDoesNotExist
+from apps.public_queries.tests.recipes import public_query_recipe
 
 
 @pytest.mark.django_db
@@ -11,3 +18,33 @@ def test_get_public_query_by_uuid(public_query):
     assert public_query_data.uuid == public_query.id
     assert public_query_data.active is False
     assert isinstance(public_query_data.name, str)
+    assert public_query_data.image is None
+
+
+@pytest.mark.django_db
+class TestGetActivePublicQueryByUUID:
+    def test_success(self):
+        public_query = public_query_recipe.make(active=True)
+        public_query_data = services.get_active_public_query_by_uuid(
+            uuid=public_query.id
+        )
+        assert isinstance(public_query_data, PublicQueryData)
+        assert public_query_data.uuid == public_query.id
+        assert isinstance(public_query_data.name, str)
+        assert public_query_data.image is None
+
+    def test_out_of_start_time(self):
+        public_query = public_query_recipe.make(
+            active=True, start_at=make_aware(datetime(2023, 1, 2))
+        )
+        with freeze_time("2023-01-01"):
+            with pytest.raises(PublicQueryDoesNotExist):
+                services.get_active_public_query_by_uuid(uuid=public_query.id)
+
+    def test_out_of_end_time(self):
+        public_query = public_query_recipe.make(
+            active=True, end_at=make_aware(datetime(2023, 1, 1))
+        )
+        with freeze_time("2023-01-02"):
+            with pytest.raises(PublicQueryDoesNotExist):
+                services.get_active_public_query_by_uuid(uuid=public_query.id)
