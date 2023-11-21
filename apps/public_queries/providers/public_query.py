6bedfce012b2b9068b02e43ff6b48049d41c41f2
Sergio Acuña <sergioacunao@protonmail.com>
2023-11-21 13:07:31 -0300

    chore(public_queries.provider.public_query): improve get_public_query_by_uuid

--- apps/public_queries/providers/public_query.py
+++ apps/public_queries/providers/public_query.py
@@ -4,9 +4,9 @@ from apps.public_queries.lib.exceptions import PublicQueryDoesNotExist
 from apps.public_queries.models import PublicQuery
 
 
-def get_public_query_by_uuid(uuid: UUID) -> PublicQuery:
+def get_public_query_by_uuid(uuid: UUID, **kwargs) -> PublicQuery:
     try:
-        public_query = PublicQuery.objects.get(id=uuid)
+        public_query = PublicQuery.objects.get(id=uuid, **kwargs)
     except PublicQuery.DoesNotExist:
         raise PublicQueryDoesNotExist
     return public_query
