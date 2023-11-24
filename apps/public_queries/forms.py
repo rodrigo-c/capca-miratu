7c5705e70d02af42f101ec5689e1cb4c18ba3323
Sergio Acuña <sergioacunao@protonmail.com>
2023-11-24 17:45:21 -0300

    chore(public_queries.forms): add methods for serializa in to dataclasses

--- apps/public_queries/forms.py
+++ apps/public_queries/forms.py
@@ -1,5 +1,9 @@
+from uuid import UUID
+
 from django import forms
 from django.forms import formset_factory
+
+from apps.public_queries.lib.dataclasses import AnswerData, ResponseData
 
 
 class ResponseForm(forms.Form):
@@ -7,9 +11,18 @@ class ResponseForm(forms.Form):
     email = forms.EmailField(required=False)
     rut = forms.CharField(max_length=10, required=False)
 
+    def get_validated_dataclass(
+        self, query_uuid: UUID, answers: list[AnswerData]
+    ) -> ResponseData:
+        return ResponseData(
+            query_uuid=query_uuid,
+            answers=answers,
+            email=self.cleaned_data.get("email") or None,
+            rut=self.cleaned_data.get("rut") or None,
+        )
+
 
 class AnswerForm(forms.Form):
-    question = forms.UUIDField(widget=forms.HiddenInput(), required=True)
     text = forms.CharField(
         required=False,
         widget=forms.Textarea(attrs={"placeholder": "Agregar comentarios"}),
@@ -22,5 +35,16 @@ class AnswerForm(forms.Form):
         if self.question_data:
             self.fields["text"].required = self.question_data.required
 
+    def get_validated_dataclass(self) -> AnswerData:
+        return AnswerData(
+            question_uuid=self.question_data.uuid,
+            text=self.cleaned_data.get("text") or None,
+        )
+
+
+def get_validated_dataclasses(formset) -> list[AnswerData]:
+    return [form.get_validated_dataclass() for form in formset.forms]
+
 
 AnswerFormSet = formset_factory(AnswerForm, extra=0)
+AnswerFormSet.get_validated_dataclasses = get_validated_dataclasses
