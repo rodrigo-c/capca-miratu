3fa3191e48b00667cd5b47f706c68165c4fbe7fb
Sergio Acuña <sergioacunao@protonmail.com>
2023-11-27 12:13:25 -0300

    chore(admin.registrations.public_queries): add url_code to django admin

--- apps/admin/registrations/public_queries.py
+++ apps/admin/registrations/public_queries.py
@@ -1,33 +1,50 @@
-from django.contrib.admin import ModelAdmin, StackedInline
+from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
 from apps.public_queries.models import Answer, PublicQuery, Question, Response
 
 
-class QuestionInLine(StackedInline):
+class QuestionInLine(admin.StackedInline):
     model = Question
     extra = 0
     ordering = ["order"]
 
 
-class PublicQueryAdmin(ModelAdmin):
-    list_display = ["name", "id", "kind", "start_at", "end_at", "active", "created_by"]
+class PublicQueryAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "view_url_code",
+        "kind",
+        "start_at",
+        "end_at",
+        "active",
+        "id",
+        "created_by",
+    ]
     search_fields = ["name"]
     ordering = ["created_at"]
     fieldsets = [
         ("Basic", {"fields": ["kind", "name", "description", "created_by"]}),
         ("Activation", {"fields": ["active", "start_at", "end_at"]}),
-        ("Visualization", {"fields": ["image"]}),
+        ("Visualization", {"fields": ["url_code", "image"]}),
     ]
     inlines = [QuestionInLine]
+    readonly_fields = ["id", "created_by", "updated_at"]
+
+    @admin.display()
+    def view_url_code(self, obj):
+        url = reverse("public_queries:submit", kwargs={"uuid": obj.url_code})
+        return format_html(f"<a href='{url}'>{obj.url_code}</a>")
 
 
-class AnswerInLine(StackedInline):
+class AnswerInLine(admin.StackedInline):
     model = Answer
     fk_name = "response"
     ordering = ["question__order"]
 
 
-class ResponseAdmin(ModelAdmin):
+class ResponseAdmin(admin.ModelAdmin):
     list_display = [
         "id",
         "query_id",
