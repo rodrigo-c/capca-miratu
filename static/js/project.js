6f53b58b0d799f6ad22fb44b63af249fd8e6feec
Sergio Acuña <sergioacunao@protonmail.com>
2023-11-28 19:11:38 -0300

    chore(static.js): add QuerySubmitManager.set_image_preview

--- static/js/project.js
+++ static/js/project.js
@@ -1,6 +1,5 @@
 /* Public query submit view */
 
-
 class QuerySubmitManager {
   constructor (
     {
@@ -17,6 +16,7 @@ class QuerySubmitManager {
     this.question_list = document.getElementsByClassName(question_class_name);
     this.hidden_class_name = hidden_class_name;
     this.back_button = document.getElementById(back_button_id)
+    this.image_fields = document.getElementsByClassName("image-input");
     this._add_listener_events();
     if (!isNaN(focus)) {
         focus = parseInt(focus)
@@ -30,6 +30,7 @@ class QuerySubmitManager {
     this.callback_show_response_form = this.callback_show_response_form.bind(this)
     this.callback_show_question_form = this.callback_show_question_form.bind(this)
     this.show_back_form = this.show_back_form.bind(this)
+    this.set_image_preview = this.set_image_preview.bind(this)
     start_button.addEventListener("click", this.callback_show_response_form, false)
     next_response_button.addEventListener("click", this.callback_show_question_form, false)
     next_response_button.question_index = 0
@@ -41,6 +42,9 @@ class QuerySubmitManager {
         }
     }
     this.back_button.addEventListener("click", this.show_back_form, false)
+    for (let image_field of this.image_fields) {
+      image_field.lastElementChild.addEventListener("change", this.set_image_preview, false)
+    }
   }
 
   hide_all () {
@@ -86,9 +90,34 @@ class QuerySubmitManager {
         this.show_detail_form()
     }
     else if (!isNaN(this.focus)) {
-
         if (this.focus == 0) {this.show_response_form()}
         else {this.show_question_form(this.focus - 1)}
+    }
+  }
+
+  set_image_preview (event) {
+    let files = event.currentTarget.files
+    let input_container = event.currentTarget.parentElement
+    let input_label = event.currentTarget.previousElementSibling
+    let payload = ""
+    for (var i = files.length - 1; i >= 0; i--) {
+      let url = URL.createObjectURL(files[i])
+      payload+=`url(${url})`
+      if (i > 0) {payload+=", "}
+    }
+    if (payload != "") {
+      input_container.style.backgroundImage = payload
+    }
+
+    if (files.length > 1) {
+      input_label.innerText = `+${files.length - 1}`
+      input_label.style.opacity = 1
+    }
+
+    if (files.length > 0) {
+      input_container.firstElementChild.style.opacity = .5
+    } else {
+      input_container.firstElementChild.style.opacity = 1
     }
   }
 }
