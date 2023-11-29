9b226172c1ea9cdef380291f1fd2922aa591b285
Sergio Acuña <sergioacunao@protonmail.com>
2023-11-29 17:12:36 -0300

    chore(public_queries.forms): improve AnswerForm client validation

--- apps/public_queries/forms.py
+++ apps/public_queries/forms.py
@@ -15,6 +15,13 @@ class ResponseForm(forms.Form):
     email = forms.EmailField(required=False)
     rut = forms.CharField(max_length=10, required=False)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["email"].widget.attrs["placeholder"] = "tu@correo.cl (Opcional)"
+        self.fields["email"].widget.attrs["autocomplete"] = "email"
+        self.fields["rut"].widget.attrs["placeholder"] = "1000000-1 (Opcional)"
+        self.fields["rut"].widget.attrs["autocomplete"] = "rut"
+
     def get_validated_dataclass(
         self, query_uuid: UUID, answers: list[AnswerData]
     ) -> ResponseData:
@@ -57,16 +64,31 @@ class AnswerForm(forms.Form):
 
     def _set_fields_by_kind(self, kind: str) -> None:
         if kind == QuestionConstants.KIND_TEXT:
-            self.fields["text"].required = self.question_data.required
-            max_length = int(self.question_data.text_max_length or 255)
-            self.fields["text"].max_length = max_length
-            self.fields["text"].validators.append(MaxLengthValidator(max_length))
-            self._hide_field("images")
+            self._set_text_answer()
+
         elif kind == QuestionConstants.KIND_IMAGE:
-            self.fields["images"].required = self.question_data.required
-            self.fields["images"].min_num = 1 if self.question_data.required else 0
-            self.fields["images"].max_num = self.question_data.max_answers
-            self._hide_field("text")
+            self._set_image_answer()
+
+    def _set_text_answer(self):
+        field = self.fields["text"]
+        field.required = self.question_data.required
+        max_length = int(self.question_data.text_max_length or 255)
+        field.max_length = max_length
+        field.validators.append(MaxLengthValidator(max_length))
+        field.widget.attrs["maxlength"] = max_length
+        field.widget.attrs["required"] = self.question_data.required
+        field.widget.attrs["minlength"] = 1 if field.required else None
+        self._hide_field("images")
+
+    def _set_image_answer(self):
+        field = self.fields["images"]
+        field.required = self.question_data.required
+        field.min_num = 1 if self.question_data.required else 0
+        field.max_num = self.question_data.max_answers
+        field.widget.attrs["maxlength"] = field.max_num
+        field.widget.attrs["minlength"] = 1 if field.required else None
+        field.widget.attrs["required"] = self.question_data.required
+        self._hide_field("text")
 
     def _hide_field(self, field: str) -> None:
         self.fields[field].disabled = True
