dc51361859c867be0767c143c2a427a001c7bef6
Sergio Acuña <sergioacunao@protonmail.com>
2023-11-30 13:21:36 -0300

    chore(public_queries.lib.constants): add QuestionConstants.KIND_SELECT

--- apps/public_queries/lib/constants.py
+++ apps/public_queries/lib/constants.py
@@ -8,7 +8,9 @@ class PublicQueryConstants:
 class QuestionConstants:
     KIND_TEXT = "TEXT"
     KIND_IMAGE = "IMAGE"
+    KIND_SELECT = "SELECT"
     KIND_CHOICES = [
         (KIND_TEXT, "Text"),
         (KIND_IMAGE, "Image"),
+        (KIND_SELECT, "Select"),
     ]
