017d5b09da2390f30ca4713589f38b3e045dc73a
Sergio Acuña <sergioacunao@protonmail.com>
2023-12-05 14:07:45 -0300

    chore(admin.public_queries): general improves

--- apps/admin/registrations/public_queries.py
+++ apps/admin/registrations/public_queries.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib import admin
+from django.contrib.gis import forms
 from django.contrib.gis.db import models
 from django.contrib.gis.forms import OSMWidget
 from django.urls import reverse
@@ -36,6 +37,10 @@ class QuestionInLine(NestedStackedInline):
     extra = 0
     ordering = ["order"]
     inlines = [QuestionOptionInLine]
+    formfield_overrides = {
+        models.TextField: {"widget": forms.Textarea(attrs={"rows": 2})},
+        models.CharField: {"widget": forms.Textarea(attrs={"rows": 2})},
+    }
 
 
 class PublicQueryAdmin(NestedModelAdmin):
@@ -52,17 +57,26 @@ class PublicQueryAdmin(NestedModelAdmin):
     search_fields = ["name"]
     ordering = ["created_at"]
     fieldsets = [
-        ("Basic", {"fields": ["kind", "name", "description", "created_by"]}),
+        (
+            "System",
+            {"fields": ["id", "created_at", "updated_at", "created_by", "url_code"]},
+        ),
+        ("Basic", {"fields": ["kind", "name", "description"]}),
         ("Activation", {"fields": ["active", "start_at", "end_at"]}),
-        ("Visualization", {"fields": ["url_code", "image"]}),
+        ("Visualization", {"fields": ["image"]}),
     ]
     inlines = [QuestionInLine]
-    readonly_fields = ["id", "created_by", "updated_at"]
+    readonly_fields = ["id", "created_at", "created_by", "updated_at"]
 
-    @admin.display()
+    # @admin.display()
     def view_url_code(self, obj):
         url = reverse("public_queries:submit", kwargs={"uuid": obj.url_code})
         return format_html(f"<a href='{url}'>{obj.url_code}</a>")
+
+    def response_post_save_add(self, request, obj):
+        obj.created_by_id = request.user.id
+        obj.save()
+        return self._response_post_save(request, obj)
 
 
 class AnswerInLine(admin.StackedInline):
@@ -70,12 +84,27 @@ class AnswerInLine(admin.StackedInline):
     extra = 0
     fk_name = "response"
     ordering = ["question__order"]
+    formfield_overrides = {models.PointField: {"widget": OSMWidget}}
+    readonly_fields = [
+        "id",
+        "created_at",
+        "updated_at",
+        "question",
+        "text",
+        "image",
+        "options",
+        "point",
+    ]
+
+    def has_add_permission(self, request, obj=None):
+        return False
 
 
 class ResponseAdmin(admin.ModelAdmin):
     list_display = [
         "id",
-        "query_id",
+        "query_code",
+        "edit_query",
         "send_at",
         "location",
         "email",
@@ -83,6 +112,32 @@ class ResponseAdmin(admin.ModelAdmin):
     ]
     inlines = [AnswerInLine]
     formfield_overrides = {models.PointField: {"widget": OSMWidget}}
+    ordering = ["query_id", "-send_at"]
+    fieldsets = [
+        ("System", {"fields": ["id", "created_at", "updated_at"]}),
+        ("Data", {"fields": ["send_at", "email", "rut", "location"]}),
+    ]
+    readonly_fields = [
+        "id",
+        "created_at",
+        "updated_at",
+        "send_at",
+        "email",
+        "rut",
+        "location",
+    ]
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def query_code(self, obj):
+        url_code = obj.query.url_code
+        url = reverse("public_queries:submit", kwargs={"uuid": url_code})
+        return format_html(f"<a href='{url}'>{url_code}</a>")
+
+    def edit_query(self, obj):
+        url = f"/admin/public_queries/publicquery/{obj.query_id}"
+        return format_html(f"<a href='{url}'>{obj.query_id}</a>")
 
 
 public_queries_tuple_models = [
