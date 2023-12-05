acbf7e458287627ccc7b14c77bee72e2ffe6347d
Sergio Acuña <sergioacunao@protonmail.com>
2023-12-05 14:08:54 -0300

    chore(public_queries.lib.constants): add QuestionOptionConstants, ResponseConstants and AnswerConstants

--- apps/public_queries/lib/constants.py
+++ apps/public_queries/lib/constants.py
@@ -1,16 +1,41 @@
+class AppConstants:
+    VERBOSE_NAME = "App Consulta Pública"
+
+
 class PublicQueryConstants:
     KIND_OPEN = "OPEN"
     KIND_CHOICES = [
         (KIND_OPEN, "Open"),
     ]
+    VERBOSE_NAME = "Consulta"
+    VERBOSE_NAME_PLURAL = "Consultas"
 
 
 class QuestionConstants:
     KIND_TEXT = "TEXT"
     KIND_IMAGE = "IMAGE"
     KIND_SELECT = "SELECT"
+    KIND_POINT = "POINT"
     KIND_CHOICES = [
         (KIND_TEXT, "Text"),
         (KIND_IMAGE, "Image"),
         (KIND_SELECT, "Select"),
+        (KIND_POINT, "Geo Point"),
     ]
+    VERBOSE_NAME = "Pregunta"
+    VERBOSE_NAME_PLURAL = "Preguntas"
+
+
+class QuestionOptionConstants:
+    VERBOSE_NAME = "Opción"
+    VERBOSE_NAME_PLURAL = "Opciones"
+
+
+class ResponseConstants:
+    VERBOSE_NAME = "Respuesta"
+    VERBOSE_NAME_PLURAL = "Respuestas"
+
+
+class AnswerConstants:
+    VERBOSE_NAME = "Respuesta a pregunta"
+    VERBOSE_NAME_PLURAL = "Respuestas a preguntas"
