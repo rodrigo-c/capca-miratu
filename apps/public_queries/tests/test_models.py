b24c4cca9825d2e5c20b206a931ff80c311649b7
Sergio Acuña <sergioacunao@protonmail.com>
2023-12-05 14:10:25 -0300

    chore(public_queries.models): add verbose names and Answer.point field

--- apps/public_queries/tests/test_models.py
+++ apps/public_queries/tests/test_models.py
@@ -4,22 +4,32 @@ import pytest
 @pytest.mark.django_db
 class TestPublicQuery:
     def test_str(self, public_query):
-        class_name = public_query.__class__.__name__
-        assert (
-            str(public_query)
-            == f"{class_name}: {public_query.name} ({public_query.id})"
-        )
+        assert str(public_query) == f"{public_query.name} ({public_query.url_code})"
 
 
 @pytest.mark.django_db
 class TestQuestion:
     def test_str(self, question):
-        class_name = question.__class__.__name__
-        assert str(question) == f"{class_name}: {question.name} ({question.id})"
+        assert str(question) == (
+            f"[{question.order}][{question.kind}] {question.name[:100]}..."
+        )
 
 
 @pytest.mark.django_db
 class TestQuestionOption:
     def test_str(self, question_option):
-        class_name = question_option.__class__.__name__
-        assert str(question_option) == f"{class_name}: {question_option.name}"
+        assert str(question_option) == f"{question_option.name} [{question_option.id}]"
+
+
+@pytest.mark.django_db
+class TestResponse:
+    def test_str(self, response):
+        assert str(response) == (
+            f"[{str(response.send_at)[:19]}] > {response.query.name[:50]}..."
+        )
+
+
+@pytest.mark.django_db
+class TestAnswer:
+    def test_str(self, answer):
+        assert str(answer) == (f"> {answer.question}")
