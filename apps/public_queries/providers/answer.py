7d5c0a48387c8a6467e5f7d78a27a31644839280
Sergio Acuña <sergioacunao@protonmail.com>
2023-12-07 16:38:54 -0300

    chore(public_queries.providers.answer): add get_total_answers_by_question_uuid, get_answers_by_question_uuid and get_total_answers_by_option_uuid

--- apps/public_queries/providers/answer.py
+++ apps/public_queries/providers/answer.py
@@ -1,6 +1,20 @@
+from uuid import UUID
+
 from apps.public_queries.models import Answer
 
 
 def bulk_create_answers(answers: list[dict]) -> list[Answer]:
     instances = [Answer(**answer) for answer in answers]
     return Answer.objects.bulk_create(objs=instances)
+
+
+def get_total_answers_by_question_uuid(question_uuid: UUID) -> int:
+    return Answer.objects.filter(question_id=question_uuid).count()
+
+
+def get_answers_by_question_uuid(question_uuid: UUID) -> list[Answer]:
+    return Answer.objects.filter(question_id=question_uuid)
+
+
+def get_total_answers_by_option_uuid(option_uuid: UUID) -> int:
+    return Answer.objects.filter(options=option_uuid).count()
