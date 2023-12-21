8852dbe8809be1e7627b69875e3b630577bfa998
Sergio Acuña <sergioacunao@protonmail.com>
2023-12-21 15:32:04 -0300

    chore(public_queries.tests.conftest/recipes): move and create make_ended_public_query to recipes

--- apps/public_queries/tests/conftest.py
+++ apps/public_queries/tests/conftest.py
@@ -1,7 +1,5 @@
 import pytest
-from django.contrib.gis.geos import Point
 
-from apps.public_queries.lib.constants import QuestionConstants
 from apps.public_queries.lib.dataclasses import QuestionData
 from apps.public_queries.tests import recipes
 from apps.public_queries.utils import create_fake_uploaded_image
@@ -67,58 +65,4 @@ def uploaded_image(field_name="images", size=(50, 50), color=(256, 0, 0)):
 
 @pytest.fixture
 def ended_public_query(uploaded_image):
-    public_query = recipes.public_query_recipe.make(active=True)
-    questions = [
-        recipes.question_recipe.make(query_id=public_query.id),
-        recipes.question_recipe.make(
-            query_id=public_query.id,
-            kind=QuestionConstants.KIND_IMAGE,
-        ),
-        recipes.question_recipe.make(
-            query_id=public_query.id,
-            kind=QuestionConstants.KIND_SELECT,
-        ),
-        recipes.question_recipe.make(
-            query_id=public_query.id,
-            kind=QuestionConstants.KIND_POINT,
-        ),
-    ]
-    options = [
-        recipes.question_option_recipe.make(question_id=questions[2].id, order=index)
-        for index in range(5)
-    ]
-    responses = []
-    for index in range(16):
-        kwargs = (
-            {}
-            if index % 2 == 0
-            else {"email": f"fake_{index}@email.com", "rut": f"100000{index}-1"}
-        )
-        response = recipes.response_recipe.make(
-            query_id=public_query.id, location=Point(1, 1), **kwargs
-        )
-        responses.append(response)
-        for question in questions:
-            answer = recipes.answer_recipe.make(
-                response_id=response.id,
-                question_id=question.id,
-                text=(
-                    f"Fake Text {index}"
-                    if question.kind == QuestionConstants.KIND_TEXT
-                    else None
-                ),
-                image=(
-                    uploaded_image
-                    if question.kind == QuestionConstants.KIND_IMAGE
-                    else None
-                ),
-                _create_files=(question.kind == QuestionConstants.KIND_IMAGE),
-                point=(
-                    Point(1, 1)
-                    if question.kind == QuestionConstants.KIND_POINT
-                    else None
-                ),
-            )
-            if question.kind == QuestionConstants.KIND_SELECT:
-                answer.options.add(options[1 if index % 2 == 0 else 2].id)
-    return public_query
+    return recipes.make_ended_public_query(uploaded_image=uploaded_image)
--- apps/public_queries/tests/recipes.py
+++ apps/public_queries/tests/recipes.py
@@ -1,6 +1,9 @@
+from django.contrib.gis.geos import Point
+
 from model_bakery.recipe import Recipe, foreign_key
 
 from apps.public_queries.lib.constants import PublicQueryConstants, QuestionConstants
+from apps.public_queries.utils import create_fake_uploaded_image
 
 public_query_recipe = Recipe(
     "public_queries.PublicQuery",
@@ -31,3 +34,62 @@ answer_recipe = Recipe(
     "public_queries.Answer",
     response=foreign_key(response_recipe),
 )
+
+
+def make_ended_public_query(uploaded_image=None):
+    uploaded_image = uploaded_image or create_fake_uploaded_image()
+    public_query = public_query_recipe.make(active=True)
+    questions = [
+        question_recipe.make(query_id=public_query.id),
+        question_recipe.make(
+            query_id=public_query.id,
+            kind=QuestionConstants.KIND_IMAGE,
+        ),
+        question_recipe.make(
+            query_id=public_query.id,
+            kind=QuestionConstants.KIND_SELECT,
+        ),
+        question_recipe.make(
+            query_id=public_query.id,
+            kind=QuestionConstants.KIND_POINT,
+        ),
+    ]
+    options = [
+        question_option_recipe.make(question_id=questions[2].id, order=index)
+        for index in range(5)
+    ]
+    responses = []
+    for index in range(16):
+        kwargs = (
+            {}
+            if index % 2 == 0
+            else {"email": f"fake_{index}@email.com", "rut": f"100000{index}-1"}
+        )
+        response = response_recipe.make(
+            query_id=public_query.id, location=Point(1, 1), **kwargs
+        )
+        responses.append(response)
+        for question in questions:
+            answer = answer_recipe.make(
+                response_id=response.id,
+                question_id=question.id,
+                text=(
+                    f"Fake Text {index}"
+                    if question.kind == QuestionConstants.KIND_TEXT
+                    else None
+                ),
+                image=(
+                    uploaded_image
+                    if question.kind == QuestionConstants.KIND_IMAGE
+                    else None
+                ),
+                _create_files=(question.kind == QuestionConstants.KIND_IMAGE),
+                point=(
+                    Point(1, 1)
+                    if question.kind == QuestionConstants.KIND_POINT
+                    else None
+                ),
+            )
+            if question.kind == QuestionConstants.KIND_SELECT:
+                answer.options.add(options[1 if index % 2 == 0 else 2].id)
+    return public_query
