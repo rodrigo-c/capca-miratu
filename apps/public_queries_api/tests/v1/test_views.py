a89006132f81f00d34eb67a40dec1240599aefdb
Sergio Acuña <sergioacunao@protonmail.com>
2023-12-28 13:36:26 -0300

    fix(public_queries_api.v1.views): fix PublicQueryMapResult

--- apps/public_queries_api/tests/v1/test_views.py
+++ apps/public_queries_api/tests/v1/test_views.py
@@ -3,10 +3,19 @@ from django.urls import reverse
 
 
 @pytest.mark.django_db
-def test_public_query_map_result(api_client, ended_public_query):
-    url = reverse(
-        "public_queries_api:v1:query-map-result-detail",
-        kwargs={"pk": ended_public_query.url_code},
-    )
-    response = api_client.get(url)
-    assert response.status_code == 200
+class TestPublicQueryMapResult:
+    def test_success(self, api_client, ended_public_query):
+        url = reverse(
+            "public_queries_api:v1:query-map-result-detail",
+            kwargs={"pk": ended_public_query.url_code},
+        )
+        response = api_client.get(url)
+        assert response.status_code == 200
+
+    def test_not_found(self, api_client, ended_public_query):
+        url = reverse(
+            "public_queries_api:v1:query-map-result-detail",
+            kwargs={"pk": 1},
+        )
+        response = api_client.get(url)
+        assert response.status_code == 404
--- apps/public_queries_api/v1/views.py
+++ apps/public_queries_api/v1/views.py
@@ -1,12 +1,22 @@
+from django.http import Http404
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 
+from apps.public_queries.lib.dataclasses import QueryMapResultData
+from apps.public_queries.lib.exceptions import PublicQueryDoesNotExist
 from apps.public_queries.services import get_public_query_map_result
 from apps.public_queries_api.v1.serializers import QueryMapResultSerializer
 
 
 class PublicQueryMapResult(ViewSet):
-    def retrieve(self, retrieve, pk) -> Response:
-        public_query_result = get_public_query_map_result(identifier=pk)
+    def retrieve(self, request, pk) -> Response:
+        public_query_result = self._get_public_query_map_result(identifier=pk)
         serializer = QueryMapResultSerializer(instance=public_query_result)
         return Response(serializer.data)
+
+    def _get_public_query_map_result(self, identifier: str) -> QueryMapResultData:
+        try:
+            public_query_result = get_public_query_map_result(identifier=identifier)
+        except PublicQueryDoesNotExist:
+            raise Http404
+        return public_query_result
