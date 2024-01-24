390f9cfab99bdfdeaf327a3fa8879974d0633b9f
Sergio Acuña <sergioacunao@protonmail.com>
2024-01-24 11:41:51 -0300

    chore(admin_api.v1.views): add links to context on PublicQueryManager viewset

--- apps/admin_api/v1/views.py
+++ apps/admin_api/v1/views.py
@@ -1,6 +1,7 @@
 from uuid import UUID
 
 from django.http import Http404
+from django.urls import reverse
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
@@ -25,6 +26,12 @@ class PublicQueryManager(ViewSet):
         result = public_queries_services.get_public_query_result(
             public_query=public_query
         )
+        kwargs = {"uuid": public_query.url_code}
+        result.links = {
+            "submit": reverse("public_queries:submit", kwargs=kwargs),
+            "map": reverse("public_queries:query-map-result", kwargs=kwargs),
+            "data": reverse("public_queries:query-data", kwargs=kwargs),
+        }
         serializer = PublicQueryResultSerializer(instance=result)
         return Response(serializer.data)
 
