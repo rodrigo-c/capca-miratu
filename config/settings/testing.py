40b626500eb029bdedd06615a90083383072d054
Sergio Acuña <sergioacunao@protonmail.com>
2024-01-29 20:21:47 -0300

    fix(config.settings.testing): add debug to template configuration

--- config/settings/testing.py
+++ config/settings/testing.py
@@ -12,3 +12,5 @@ TEST_RUNNER = "django.test.runner.DiscoverRunner"
 
 PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
 EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
+
+TEMPLATES[0]["OPTIONS"]["debug"] = True  # noqa
