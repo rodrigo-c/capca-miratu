db706f2e22f6897301fbe381bac31d48980af331
Sergio Acuña <sergioacunao@protonmail.com>
2024-01-30 19:04:53 -0300

    chore(admin_api.v1.views): add PublicQueryManager.create

--- apps/admin_api/tests/v1/test_views.py
+++ apps/admin_api/tests/v1/test_views.py
@@ -1,7 +1,14 @@
+from dataclasses import asdict
+from unittest import mock
+
 import pytest
 from django.urls import reverse
 
-from apps.public_queries.tests.recipes import public_query_recipe
+from apps.public_queries.lib.constants import CreatePublicQueryConstants
+from apps.public_queries.tests.recipes import (
+    make_public_query_data,
+    public_query_recipe,
+)
 
 
 @pytest.mark.django_db
@@ -32,3 +39,46 @@ class TestPublicQueryManager:
         url = reverse(f"{self.base_pattern}-detail", kwargs={"pk": "x"})
         response = api_client.get(url)
         assert response.status_code == 404
+
+    def _get_create_data(self) -> dict:
+        public_query_data = make_public_query_data()
+        data = asdict(public_query_data)
+        for field in list(data):
+            if data[field] is None:
+                data.pop(field)
+        for question in data["questions"]:
+            question["options"] = question["options"] or []
+        return data
+
+    def test_create_success(self, api_client, user):
+        api_client.force_login(user)
+        url = reverse(f"{self.base_pattern}-list")
+        data = self._get_create_data()
+        response = api_client.post(url, data=data, format="json")
+        assert response.status_code == 200
+        url = reverse(
+            f"{self.base_pattern}-detail", kwargs={"pk": response.data["uuid"]}
+        )
+        assert api_client.get(url).status_code == 200
+
+    @mock.patch("apps.public_queries.providers.public_query.create_public_query")
+    def test_create_error(self, mock_create_public_query, api_client, user):
+        api_client.force_login(user)
+        url = reverse(f"{self.base_pattern}-list")
+        data = self._get_create_data()
+        mock_create_public_query.side_effect = ValueError
+        response = api_client.post(url, data=data, format="json")
+        assert response.status_code == 400
+
+    def test_create_with_invalid(self, api_client, user):
+        api_client.force_login(user)
+        url = reverse(f"{self.base_pattern}-list")
+        data = self._get_create_data()
+        data["start_at"] = "2000-02-01 20:00:00"
+        data["end_at"] = "2000-01-01 20:00:00"
+        response = api_client.post(url, data=data, format="json")
+        assert response.status_code == 400
+        assert (
+            str(response.data["non_field_errors"][0])
+            == CreatePublicQueryConstants.INVALID_START_END_AT
+        )
