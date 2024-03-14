65b0a9d2c4332eafaee744f0a6a6eef5e4fbcfea
Sergio Acuña <sergioacunao@protonmail.com>
2024-03-14 16:49:13 -0300

    chore(static.js): remove unused js

--- static/js/project.js
+++ static/js/project.js
@@ -1,26 +1,5 @@
 import {QuerySubmitEngine} from "./submit/engine.js"
-import {QueryMapResultEngine} from "./result_map/engine.js"
-import {QueryDataResultEngine} from "./result_data/engine.js"
 import {AdminEngine} from "./admin/engine.js"
 
 
-// need https://leafletjs.com/
-function set_answer_result_map (id, dataset) {
-  let latlng = L.latLng(-33.447869, -70.668423)
-  let map = L.map(id).setView(latlng, 9)
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-  }).addTo(map);
-  map.attributionControl.setPrefix("")
-
-  for (let answer of dataset) {
-    if (answer.point) {
-      let marker = L.marker(answer.point).addTo(map)
-      let message = `<b>Enviado en: </b> ${new Date(answer.send_at)}`
-      marker.bindPopup(message)
-    }
-  }
-  return map
-}
-
-export {AdminEngine, QuerySubmitEngine, QueryDataResultEngine, QueryMapResultEngine, set_answer_result_map}
+export {AdminEngine, QuerySubmitEngine}
