15faa39b4ade2f1a40f1caff9d436498630a5425
Sergio Acuña <sergioacunao@protonmail.com>
2024-03-27 12:10:30 -0300

    chore(public_queries.storages): add storage path functions for models

---
+++ apps/public_queries/storages.py
@@ -0,0 +1,20 @@
+def get_public_query_image_path(instance, filename) -> str:
+    return f"public_queries/{instance.url_code}/images/{filename}"
+
+
+def get_public_query_answer_image_path(instance, filename) -> str:
+    url_code = instance.response.query.url_code
+    return f"public_queries/{url_code}/answers/{instance.response_id}/{filename}"
+
+
+def get_public_query_question_image_path(instance, filename) -> str:
+    url_code = instance.query.url_code
+    return f"public_queries/{url_code}/questions/{instance.id}/{filename}"
+
+
+def get_public_query_question_option_image_path(instance, filename) -> str:
+    url_code = instance.question.query.url_code
+    return (
+        f"public_queries/{url_code}/questions/"
+        f"{instance.question_id}/options/{instance.id}/{filename}"
+    )
