df1cc639f57189322ae74d2b29225e6910d3c11d
Sergio Acuña <sergioacunao@protonmail.com>
2024-06-13 14:43:56 -0400

    chore(js.admin.queries.results): use of thumb on image answers

--- static/js/admin/queries/results.js
+++ static/js/admin/queries/results.js
@@ -559,8 +559,8 @@ class QueryResultManager {
     let final_value = value
 
     if (question.kind == "IMAGE" && value) {
-      final_value = `<div class="query-datable-image" style="background-image: url('${value}')"></div>`
-      final_value = `<a href="${value}" target="_blank">${final_value}</a>`
+      final_value = `<div class="query-datable-image" style="background-image: url('${value.thumb}')"></div>`
+      final_value = `<a href="${value.original}" target="_blank">${final_value}</a>`
     } else if (question.kind == "POINT" && value) {
       let query_url_code = this.data.query.url_code
       let link = item.visible === "True"? `href="?f=query-map&k=${query_url_code}&r=${item.uuid}"`: ""
