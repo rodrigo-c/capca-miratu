ab3169f708ac9ba9dfb900fd30a481fadb392597
Sergio Acuña <sergioacunao@protonmail.com>
2024-09-04 13:58:11 -0400

    chore(config.settings.production): add allowed_host and crf_tristed_origins

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,7 +7,8 @@ ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
 EMAIL_HOST = env("EMAIL_HOST", default=None)
 DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default="no-reply@vision.cegirapp.cl")
 
-
+ALLOWED_HOSTS = ["vision.cegirapp.com"]
+CSRF_TRUSTED_ORIGINS = ["https://vision.cegirapp.com"]
 if EMAIL_HOST:
     EMAIL_PORT = env("EMAIL_PORT")
     EMAIL_USE_TLS = True
