e0a070f394942354429f6640ea5325eceee28031
Sergio Acuña <sergioacunao@protonmail.com>
2024-09-06 11:11:29 -0400

    fix(config.storage): fix end line

--- config/storages.py
+++ config/storages.py
@@ -3,11 +3,11 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = "static"
-    default_acl = 'public-read'
+    default_acl = "public-read"
 
 
 class PublicMediaStorage(S3Boto3Storage):
     location = "media"
-    default_acl = 'private'
+    default_acl = "private"
     file_overwrite = False
     custom_domain = False
