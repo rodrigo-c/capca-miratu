e0f870a2e925d042bd248cae64eeda86ebec45e8
Sergio Acuña <sergioacunao@protonmail.com>
2024-09-09 10:53:16 -0300

    chore(config.settings.production): add ALLOWED_HOSTS andCSRF_TRUSTED_ORIGINS as env vars

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,8 +7,10 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 EMAIL_HOST = env("EMAIL_HOST", default=None)
 DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default="no-reply@vision.cegirapp.cl")
 
-ALLOWED_HOSTS = ["vision.cegirapp.com"]
-CSRF_TRUSTED_ORIGINS = ["https://vision.cegirapp.com"]
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["vision.cegirapp.com"])
+CSRF_TRUSTED_ORIGINS = env.list(
+    "DJANGO_CSRF_TRUSTED_ORIGINS", default=["https://vision.cegirapp.com"]
+)
 if EMAIL_HOST:
     EMAIL_PORT = env("EMAIL_PORT")
     EMAIL_USE_TLS = True
