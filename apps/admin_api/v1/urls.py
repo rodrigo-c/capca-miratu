4940c5832517af1c30ce6e1d45e070956a6a6264
Sergio Acuña <sergioacunao@protonmail.com>
2024-09-09 10:01:24 -0300

    chore(admin_api.v1.views): add PublicQueryAPI

--- apps/admin_api/v1/urls.py
+++ apps/admin_api/v1/urls.py
@@ -1,10 +1,11 @@
-from apps.admin_api.v1.views import PublicQueryManager
+from apps.admin_api.v1.views import PublicQueryAPI, PublicQueryManager
 from apps.utils.api import get_api_router
 
 app_name = "v1"
 
 router = get_api_router()
 router.register("public-query", PublicQueryManager, basename="public-query")
+router.register("public-query-api", PublicQueryAPI, basename="public-query-api")
 
 
 urlpatterns = router.urls
--- apps/admin_api/v1/views.py
+++ apps/admin_api/v1/views.py
@@ -1,3 +1,4 @@
+import json
 import re
 from io import BytesIO
 from uuid import UUID
@@ -5,6 +6,8 @@ from uuid import UUID
 from django.http import FileResponse, Http404
 from django.urls import reverse
 from rest_framework import status as response_status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -38,9 +41,137 @@ from apps.public_queries.lib.exceptions import (
 )
 
 
-class PublicQueryManager(ViewSet):
+class PublicQueryBase(ViewSet):
     permission_classes = [IsAuthenticated]
 
+    def filter_by_user(
+        self,
+        public_queries: list,
+    ) -> list:
+        filtered_queries = filter(
+            lambda query: (
+                query.created_by_email == self.request.user.email
+                or self.request.user.is_superuser
+            ),
+            public_queries,
+        )
+        return list(filtered_queries)
+
+    def get_public_query(self, identifier: str | UUID) -> PublicQueryData:
+        try:
+            public_query = public_queries_services.get_public_query(
+                identifier=identifier
+            )
+            if not self.filter_by_user(public_queries=[public_query]):
+                raise PublicQueryDoesNotExist
+        except PublicQueryDoesNotExist:
+            raise Http404
+        return public_query
+
+    def _get_public_query_data_result(self, identifier: str) -> dict:
+        try:
+            public_query_result = (
+                public_queries_services.get_public_query_responses_data(
+                    identifier=identifier
+                )
+            )
+            created_by_email = public_query_result["query"]["created_by_email"]
+            if (
+                not self.request.user.is_superuser
+                and self.request.user.email != created_by_email
+            ):
+                raise PublicQueryDoesNotExist
+        except PublicQueryDoesNotExist:
+            raise Http404
+        return public_query_result
+
+    def _get_verbose_fields(self, dataset: dict) -> list:
+        verbose_fields = {}
+        for field in dataset["fields"]:
+            verbose = PublicQueryDataResultConstants.VERBOSE_FIELDS.get(field, field)
+            if "pregunta_" in field:
+                verbose = field.replace("_", " ").capitalize()
+            verbose_fields[field] = verbose
+        return verbose_fields
+
+    def get_public_query_map_result(self, identifier: str) -> QueryMapResultData:
+        try:
+            public_query_result = public_queries_services.get_public_query_map_result(
+                identifier=identifier
+            )
+            if not self.filter_by_user(public_queries=[public_query_result.query]):
+                raise PublicQueryDoesNotExist
+        except PublicQueryDoesNotExist:
+            raise Http404
+        return public_query_result
+
+    def _get_gsojson_file(self, public_query_result: QueryMapResultData) -> BytesIO:
+        query = public_query_result.query
+        query_properties = {
+            "name": query.name,
+            "description": query.description,
+            "start_at": str(query.start_at),
+            "end_at": str(query.end_at),
+            "questions": {
+                question.index: {
+                    "name": question.name,
+                    "description": question.description,
+                }
+                for question in query.questions
+                if question.kind == QuestionConstants.KIND_POINT
+            },
+        }
+        features = []
+        for point_data in public_query_result.point_list:
+            if point_data.question_index:
+                point = Point(point_data.location)
+                response = point_data.response
+                feature_properties = {
+                    "question_index": point_data.question_index,
+                    "response": {
+                        "send_at": str(response.send_at) if response.send_at else None,
+                        "rut": str(response.rut) if response.rut else None,
+                        "email": str(response.email) if response.email else None,
+                    },
+                }
+                feature = Feature(
+                    id=f"{response.uuid}::{point_data.question_index}",
+                    geometry=point,
+                    properties=feature_properties,
+                )
+                features.append(feature)
+        feature_collection = FeatureCollection(
+            features,
+            id=query.url_code,
+            properties=query_properties,
+        )
+        return str(feature_collection)
+
+
+class PublicQueryAPI(PublicQueryBase):
+    authentication_classes = [TokenAuthentication]
+
+    def list(self, request) -> Response:
+        data_list = self.filter_by_user(
+            public_queries=public_queries_services.get_public_query_list()
+        )
+        serializer = PublicQuerySerializer(many=True, instance=data_list)
+        return Response({"list": serializer.data})
+
+    def retrieve(self, request, pk=None) -> Response:
+        public_query_result = self._get_public_query_data_result(identifier=pk)
+        verbose_fields = self._get_verbose_fields(dataset=public_query_result)
+        public_query_result["verbose_fields"] = verbose_fields
+        return Response(public_query_result)
+
+    @action(detail=True, methods=["get"])
+    def geojson(self, request, pk) -> Response:
+        public_query_result = self.get_public_query_map_result(identifier=pk)
+        geojson = self._get_gsojson_file(public_query_result=public_query_result)
+        return Response(json.loads(geojson))
+
+
+class PublicQueryManager(PublicQueryBase):
     def list(self, request) -> Response:
         data_list = self.filter_by_user(
             public_queries=public_queries_services.get_public_query_list()
@@ -96,6 +227,25 @@ class PublicQueryManager(ViewSet):
             if public_queries_services.delete_public_query(uuid=public_query.uuid):
                 return Response({}, status=response_status.HTTP_202_ACCEPTED)
         return Response({}, status=response_status.HTTP_406_NOT_ACCEPTABLE)
+
+    @action(detail=False, methods=["get"])
+    def api_config(self, request) -> Response:
+        token, _ = Token.objects.get_or_create(user=request.user)
+        paths = {
+            "list": reverse("admin_api:v1:public-query-api-list"),
+            "results": reverse(
+                "admin_api:v1:public-query-api-detail", kwargs={"pk": "--url_code--"}
+            ),
+            "geojson": reverse(
+                "admin_api:v1:public-query-api-geojson", kwargs={"pk": "--url_code--"}
+            ),
+        }
+        api_config = {
+            "user": request.user.email,
+            "token": token.key,
+            "paths": paths,
+        }
+        return Response(api_config)
 
     @action(detail=False, methods=["post"])
     def update_question_image(self, request) -> Response:
@@ -202,67 +352,6 @@ class PublicQueryManager(ViewSet):
         response["Content-Disposition"] = f"attachment; filename={filename}"
         return response
 
-    def filter_by_user(
-        self,
-        public_queries: list,
-    ) -> list:
-        filtered_queries = filter(
-            lambda query: (
-                query.created_by_email == self.request.user.email
-                or self.request.user.is_superuser
-            ),
-            public_queries,
-        )
-        return list(filtered_queries)
-
-    def get_public_query(self, identifier: str | UUID) -> PublicQueryData:
-        try:
-            public_query = public_queries_services.get_public_query(
-                identifier=identifier
-            )
-            if not self.filter_by_user(public_queries=[public_query]):
-                raise PublicQueryDoesNotExist
-        except PublicQueryDoesNotExist:
-            raise Http404
-        return public_query
-
-    def get_public_query_map_result(self, identifier: str) -> QueryMapResultData:
-        try:
-            public_query_result = public_queries_services.get_public_query_map_result(
-                identifier=identifier
-            )
-            if not self.filter_by_user(public_queries=[public_query_result.query]):
-                raise PublicQueryDoesNotExist
-        except PublicQueryDoesNotExist:
-            raise Http404
-        return public_query_result
-
-    def _get_public_query_data_result(self, identifier: str) -> dict:
-        try:
-            public_query_result = (
-                public_queries_services.get_public_query_responses_data(
-                    identifier=identifier
-                )
-            )
-            created_by_email = public_query_result["query"]["created_by_email"]
-            if (
-                not self.request.user.is_superuser
-                and self.request.user.email != created_by_email
-            ):
-                raise PublicQueryDoesNotExist
-        except PublicQueryDoesNotExist:
-            raise Http404
-        return public_query_result
-
-    def _get_verbose_fields(self, dataset: dict) -> list:
-        verbose_fields = {}
-        for field in dataset["fields"]:
-            verbose = PublicQueryDataResultConstants.VERBOSE_FIELDS.get(field, field)
-            if "pregunta_" in field:
-                verbose = field.replace("_", " ").capitalize()
-            verbose_fields[field] = verbose
-        return verbose_fields
-
     def _get_simpletables_config(self, public_query, fields) -> dict:
         columns = []
         headings = []
@@ -323,45 +412,3 @@ class PublicQueryManager(ViewSet):
         wb.save(excel_file)
         excel_file.seek(0)
         return excel_file
-
-    def _get_gsojson_file(self, public_query_result: QueryMapResultData) -> BytesIO:
-        query = public_query_result.query
-        query_properties = {
-            "name": query.name,
-            "description": query.description,
-            "start_at": str(query.start_at),
-            "end_at": str(query.end_at),
-            "questions": {
-                question.index: {
-                    "name": question.name,
-                    "description": question.description,
-                }
-                for question in query.questions
-                if question.kind == QuestionConstants.KIND_POINT
-            },
-        }
-        features = []
-        for point_data in public_query_result.point_list:
-            if point_data.question_index:
-                point = Point(point_data.location)
-                response = point_data.response
-                feature_properties = {
-                    "question_index": point_data.question_index,
-                    "response": {
-                        "send_at": str(response.send_at) if response.send_at else None,
-                        "rut": str(response.rut) if response.rut else None,
-                        "email": str(response.email) if response.email else None,
-                    },
-                }
-                feature = Feature(
-                    id=f"{response.uuid}::{point_data.question_index}",
-                    geometry=point,
-                    properties=feature_properties,
-                )
-                features.append(feature)
-        feature_collection = FeatureCollection(
-            features,
-            id=query.url_code,
-            properties=query_properties,
-        )
-        return str(feature_collection)
